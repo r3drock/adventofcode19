1efddf9157fa4f6f538a6fae04187c8a347ce638
r3drock <philipp.koppenstein@udo.edu>
2019-12-02 16:30:48 +0100

    refactor

--- 1/solve.rs
+++ 1/solve.rs
@@ -3,13 +3,8 @@ use std::fs;
 fn main() {
     let data = fs::read_to_string("data")
         .expect("Something went wrong reading the file");
-    let lines = data.split('\n');
-    let mut sum = 0;
-    for line in lines {
-        sum += match line.parse::<i32>() {
-            Ok(i) => i/3-2,
-            Err(_) => 0,
-        };
-    }
+    let masses : Vec<i64> = data.split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect();
+    let fuel_requirements : Vec<i64> = masses.into_iter().map(|x| x/3 - 2).collect();
+    let sum : i64 = fuel_requirements.into_iter().sum();
     println!("{}", sum);
 }
