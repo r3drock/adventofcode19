cc527b1b5995b0324e73596e8f1377dc6974a42d
r3drock <philipp.koppenstein@udo.edu>
2019-12-03 14:47:57 +0100

    get rid of binary files

--- 1Dec/solve2.rs
+++ 1Dec/solve2.rs
@@ -1,19 +1,18 @@
 use std::fs;
 
-fn calculate_fuel_needed(a : i64) -> i64 {
-    let b = a / 3 - 2;
-    return match b {
-        std::i64::MIN ..= 0 => 0,
-        b  => b + calculate_fuel_needed(b),
-    };
+fn calculate_fuel_needed(fuel : i64) -> i64 {
+    let fuel = fuel / 3 - 2;
+    if fuel > 0 {
+        fuel + calculate_fuel_needed(fuel)
+    } else { 0 }
 }
 
 fn main() {
     let data = fs::read_to_string("data")
         .expect("Something went wrong reading the file");
-    let lines = data.split('\n');
     let mut sum = 0;
-    for line in lines {
+
+    for line in data.split('\n') {
         sum += match line.parse::<i64>() {
             Ok(i) => calculate_fuel_needed(i),
             Err(_) => 0,
