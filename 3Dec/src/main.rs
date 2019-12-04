6a3e1ac4a4ac5cec2f1bc2f7393ee154e3625e46
r3drock <philipp.koppenstein@udo.edu>
2019-12-04 21:16:20 +0100

    finish parsing

--- 3Dec/src/main.rs
+++ 3Dec/src/main.rs
@@ -1,26 +1,52 @@
-fn test_password(mut value: u32) -> u32 {
-    let mut adjacency = false;
+use std::fs;
 
-    let mut prev = 10;
-    //go from right to left digit
-    while value > 0 {
-        let current_digit = value % 10;
-        if prev == current_digit {
-            adjacency = true;
-        }
-        if prev < current_digit {
-            return 0;
-        }
-        prev = current_digit;
-        value /= 10;
-    }
-    if adjacency {1} else {0}
+//fn parse_input(cable1: Vec<(&str,&str)>, cable2: Vec<(&str,&str)>) -> (Vec<(& str,& str)>,Vec<(& str,& str)>) {
+//    let data = fs::read_to_string("data")
+//        .expect("Something went wrong reading the file");
+//    let mut lines = data.split('\n');
+//    let cable1_directions:String = lines.next()
+//        .expect("Line 1 is missing.").to_string();
+//    let cable2_directions:String = lines.next()
+//        .expect("Line 2 is missing.").to_string();
+//    for direction in cable1_directions.split(',') {
+//        cable1.push((&direction[..1], &direction[1..]));
+//    }
+//    for direction in cable2_directions.split(',') {
+//        cable2.push((&direction[..1], &direction[1..]));
+//    }
+//    (cable1,cable2)
+//}
+
+fn calculate_dimensions (cable: Vec<(&str, &str)>) {
+    
 }
 
 fn main() {
-    let mut count = 0;
-    for value in 245182..790572 {
-        count += test_password(value);
+    let mut cable1: Vec<(&str, &str)> = Vec::new();
+    let mut cable2: Vec<(&str, &str)> = Vec::new();
+    let data = fs::read_to_string("data")
+        .expect("Something went wrong reading the file");
+    let mut lines = data.split('\n');
+    let cable1_directions:String = lines.next()
+        .expect("Line 1 is missing.").to_string();
+    let cable2_directions:String = lines.next()
+        .expect("Line 2 is missing.").to_string();
+    for direction in cable1_directions.split(',') {
+        cable1.push((&direction[..1], &direction[1..]));
     }
-    println!("{}", count);
+    for direction in cable2_directions.split(',') {
+        cable2.push((&direction[..1], &direction[1..]));
+    }
+
+
+
+
+
+    for (direction, length) in cable1 {
+        println!("({}, {})", direction, length);
+    }
+        println!("--------------------");
+    for (direction, length) in cable2 {
+        println!("({}, {})", direction, length);
+    }
 }
