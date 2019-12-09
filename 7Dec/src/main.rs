95bc33d7fe729e60c8b87ff5d56f93721431ed90
r3drock <philipp.koppenstein@udo.edu>
2019-12-09 22:55:49 +0100

    refactor Day 7

--- 7Dec/src/main.rs
+++ 7Dec/src/main.rs
@@ -3,6 +3,14 @@ use std::fs;
 use std::convert::TryFrom;
 use permutohedron::Heap;
 
+#[derive(Debug)]
+struct Amplifier {
+    ip: usize,
+    inputbuffer: Vec<isize>,
+    program: Vec<isize>,
+}
+
+
 fn read_data() -> Vec<isize> {
     let mut program: Vec<isize> = Vec::new();
     let data = fs::read_to_string("data")
@@ -45,108 +53,117 @@ fn access(mode: usize, index: usize, program: &Vec<isize>) -> isize {
     if mode == 0 {program[conv(program[index])]} else {program[index]}
 }
 
-fn run_program(mut program: Vec<isize>, input_vec: Vec<isize>, terminated: &mut bool) -> Option<isize> {
-    let mut output: Option<isize> = None;
-    let mut input_vec_iterator = input_vec.iter();
-    let size = program.len();
-    let mut ip: usize = 0;
-    while ip < size - (size % 4) {
-        let (mode1, mode2, mode3) = get_modes(conv(program[ip]));
-
-        match get_opcode(conv(program[ip])) {
-            //add
-            1 => {
-                let index_to_overwrite = if mode3 == 0 {conv(program[ip+3])} else {ip+3};
-                program[index_to_overwrite] = access(mode1, ip+1, &program) +
-                                              access(mode2, ip+2, &program);
-                ip += 4;
-            }
-
-            //mul
-            2 => {
-                let index_to_overwrite = if mode3 == 0 {conv(program[ip+3])} else {ip+3};
-                program[index_to_overwrite] = access(mode1, ip+1, &program) *
-                                              access(mode2, ip+2, &program);
-                ip += 4;
-            }
-
-            //input
-            //reads from input_vec and if it is not present from stdin
-            3 => {
-                let input: isize = match input_vec_iterator.next() {
-                    Some(num) => *num,
-                    None => {
-                        let mut input_string = String::new();
-                        println!("Please input a number.");
-                        io::stdin().read_line(&mut input_string)
-                            .expect("error reading line");
-
-                        match input_string.trim().parse() {
-                            Ok(num) => num,
-                            Err(_) => continue,
-                        }
-                    },
-                };
-
-                let index_to_overwrite = if mode1 == 0 {conv(program[ip+1])} else {ip+1};
-                program[index_to_overwrite] = input;
-                ip += 2;
-            },
-
-            //output
-            4 => {
-                output = Some(access(mode1, ip+1, &program));
-                //println!("{}", output.unwrap());
-                ip += 2;
-            },
-
-            //jump-if-true
-            5 => {
-                ip = if access(mode1, ip+1, &program) != 0 {
-                    conv(access(mode2, ip+2, &program))
-                } else {
-                    ip + 3
-                };
-            },
-
-            //jump-if-false
-            6 => {
-                ip = if access(mode1, ip+1, &program) == 0 {
-                    conv(access(mode2, ip+2, &program))
-                } else {
-                    ip + 3
-                };
-            },
-
-            //less than
-            7 => {
-                let index_to_overwrite = if mode3 == 0 {conv(program[ip+3])} else {ip+3};
-                program[index_to_overwrite] =
-                    if access(mode1, ip+1, &program) < access(mode2, ip+2, &program) {
-                    1
-                } else {
-                    0
-                };
-                ip += 4;
-            }
-
-            //equals
-            8 => {
-                let index_to_overwrite = if mode3 == 0 {conv(program[ip+3])} else {ip+3};
-                program[index_to_overwrite] =
-                    if access(mode1, ip+1, &program) == access(mode2, ip+2, &program) {
-                    1
-                } else {
-                    0
-                };
-                ip += 4;
-            }
-            99 => break,//{*terminated = true; break;},
-            _ => panic!("illegal opcode"),
-        };
-
+impl Amplifier {
+    fn new(program: Vec<isize>, input: Vec<isize>) -> Amplifier {
+        Amplifier { inputbuffer: input, program: program, ip : 0 }
     }
-    output
+
+    fn push_input(&mut self, input: isize) {
+        self.inputbuffer.push(input);
+    }
+
+    fn run_program(&mut self, terminated: &mut bool) -> Option<isize> {
+        let mut output: Option<isize> = None;
+        let size = self.program.len();
+        while self.ip < size - (size % 4) {
+            let (mode1, mode2, mode3) = get_modes(conv(self.program[self.ip]));
+
+            match get_opcode(conv(self.program[self.ip])) {
+                //add
+                1 => {
+                    let index_to_overwrite = if mode3 == 0 {conv(self.program[self.ip+3])} else {self.ip+3};
+                    self.program[index_to_overwrite] = access(mode1, self.ip+1, &self.program) +
+                                                  access(mode2, self.ip+2, &self.program);
+                    self.ip += 4;
+                }
+
+                //mul
+                2 => {
+                    let index_to_overwrite = if mode3 == 0 {conv(self.program[self.ip+3])} else {self.ip+3};
+                    self.program[index_to_overwrite] = access(mode1, self.ip+1, &self.program) *
+                                                  access(mode2, self.ip+2, &self.program);
+                    self.ip += 4;
+                }
+
+                //input
+                //reads from input_vec and if it is not present from stdin
+                3 => {
+                    let input: isize = match self.inputbuffer.first() {
+                        Some(num) => *num,
+                        None => {
+                            let mut input_string = String::new();
+                            println!("Please input a number.");
+                            io::stdin().read_line(&mut input_string)
+                                .expect("error reading line");
+
+                            match input_string.trim().parse() {
+                                Ok(num) => num,
+                                Err(_) => continue,
+                            }
+                        },
+                    };
+
+                    let index_to_overwrite = if mode1 == 0 {conv(self.program[self.ip+1])} else {self.ip+1};
+                    self.program[index_to_overwrite] = input;
+                    self.ip += 2;
+                },
+
+                //output
+                4 => {
+                    output = Some(access(mode1, self.ip+1, &self.program));
+                    //println!("{}", output.unwrap());
+                    self.ip += 2;
+                    break;
+                },
+
+                //jump-if-true
+                5 => {
+                    self.ip = if access(mode1, self.ip+1, &self.program) != 0 {
+                        conv(access(mode2, self.ip+2, &self.program))
+                    } else {
+                        self.ip + 3
+                    };
+                },
+
+                //jump-if-false
+                6 => {
+                    self.ip = if access(mode1, self.ip+1, &self.program) == 0 {
+                        conv(access(mode2, self.ip+2, &self.program))
+                    } else {
+                        self.ip + 3
+                    };
+                },
+
+                //less than
+                7 => {
+                    let index_to_overwrite = if mode3 == 0 {conv(self.program[self.ip+3])} else {self.ip+3};
+                    self.program[index_to_overwrite] =
+                        if access(mode1, self.ip+1, &self.program) < access(mode2, self.ip+2, &self.program) {
+                        1
+                    } else {
+                        0
+                    };
+                    self.ip += 4;
+                }
+
+                //equals
+                8 => {
+                    let index_to_overwrite = if mode3 == 0 {conv(self.program[self.ip+3])} else {self.ip+3};
+                    self.program[index_to_overwrite] =
+                        if access(mode1, self.ip+1, &self.program) == access(mode2, self.ip+2, &self.program) {
+                        1
+                    } else {
+                        0
+                    };
+                    self.ip += 4;
+                }
+                99 => {*terminated = true; break;},
+                a => panic!("illegal opcode {}", a),
+            };
+
+        }
+        output
+    }
 }
 
 fn print_program(program: &Vec<isize>) {
@@ -191,63 +208,75 @@ fn part1() {
     let mut terminated = false;
     let mut max = 0;
     for input in permutations {
-        let mut output = run_program(program.clone(), vec![input[0],0], &mut terminated).unwrap();
-        output = run_program(program.clone(), vec![input[1], output], &mut terminated).unwrap();
-        output = run_program(program.clone(), vec![input[2], output], &mut terminated).unwrap();
-        output = run_program(program.clone(), vec![input[3], output], &mut terminated).unwrap();
-        output = run_program(program.clone(), vec![input[4], output], &mut terminated).unwrap();
+        let mut amplifier_a = Amplifier::new(program.clone(), vec![input[0],0]); 
+        let mut amplifier_b = Amplifier::new(program.clone(), vec![input[1]]); 
+        let mut amplifier_c = Amplifier::new(program.clone(), vec![input[2]]); 
+        let mut amplifier_d = Amplifier::new(program.clone(), vec![input[3]]); 
+        let mut amplifier_e = Amplifier::new(program.clone(), vec![input[4]]); 
+
+        let output = amplifier_a.run_program(&mut terminated).unwrap();
+        amplifier_b.push_input(output);
+        let output = amplifier_b.run_program(&mut terminated).unwrap();
+        amplifier_c.push_input(output);
+        let output = amplifier_c.run_program(&mut terminated).unwrap();
+        amplifier_d.push_input(output);
+        let output = amplifier_d.run_program(&mut terminated).unwrap();
+        amplifier_e.push_input(output);
+        let output = amplifier_e.run_program(&mut terminated).unwrap();
+
+        println!("output: {}", output);
         if output > max {max = output;};
     }
     println!("Highest signal that can be sent to the thrusters: {}", max);
 }
 
-fn part2() {
-    let program = read_data();
-
-    let mut data = vec![5, 6, 7, 8, 9];
-    let heap = Heap::new(&mut data);
-
-    let mut permutations = Vec::new();
-    for data in heap {
-        permutations.push(data.clone());
-    }
-
-    let mut terminated = false;
-    let mut max = 0;
-    for input in permutations {
-            let program_A = program.clone();
-            let program_B = program.clone();
-            let program_C = program.clone();
-            let program_D = program.clone();
-            let program_E = program.clone();
-            let mut output = run_program(program.clone(), vec![input[0], 0], &mut terminated).unwrap();
-            output = run_program(program.clone(), vec![input[1], output], &mut terminated).unwrap();
-            let output = run_program(program.clone(), vec![input[2], output], &mut terminated).unwrap();
-            let mut output = run_program(program.clone(), vec![input[3], output], &mut terminated).unwrap();
-            let last_amplifier_output = run_program(program.clone(), vec![input[4], output], &mut terminated).unwrap();
-            if terminated {
-                if last_amplifier_output > max {max = last_amplifier_output;};
-                continue;
-            }
-            loop {
-                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
-                if terminated {break;}
-                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
-                if terminated {break;}
-                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
-                if terminated {break;}
-                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
-                if terminated {break;}
-                let last_amplifier_output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
-                output = last_amplifier_output;
-                if terminated {break;}
-
-            }
-            if last_amplifier_output > max {max = last_amplifier_output;};
-        }
-    println!("Highest signal that can be sent to the thrusters: {}", max);
-}
+//fn part2() {
+//    let program = read_data();
+//
+//    let mut data = vec![5, 6, 7, 8, 9];
+//    let heap = Heap::new(&mut data);
+//
+//    let mut permutations = Vec::new();
+//    for data in heap {
+//        permutations.push(data.clone());
+//    }
+//
+//    let mut terminated = false;
+//    let mut max = 0;
+//    for input in permutations {
+//            let program_A = program.clone();
+//            let program_B = program.clone();
+//            let program_C = program.clone();
+//            let program_D = program.clone();
+//            let program_E = program.clone();
+//            let mut output = run_program(program.clone(), vec![input[0], 0], &mut terminated).unwrap();
+//            output = run_program(program.clone(), vec![input[1], output], &mut terminated).unwrap();
+//            let output = run_program(program.clone(), vec![input[2], output], &mut terminated).unwrap();
+//            let mut output = run_program(program.clone(), vec![input[3], output], &mut terminated).unwrap();
+//            let last_amplifier_output = run_program(program.clone(), vec![input[4], output], &mut terminated).unwrap();
+//            if terminated {
+//                if last_amplifier_output > max {max = last_amplifier_output;};
+//                continue;
+//            }
+//            loop {
+//                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
+//                if terminated {break;}
+//                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
+//                if terminated {break;}
+//                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
+//                if terminated {break;}
+//                output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
+//                if terminated {break;}
+//                let last_amplifier_output = run_program(program.clone(), vec![output], &mut terminated).unwrap();
+//                output = last_amplifier_output;
+//                if terminated {break;}
+//
+//            }
+//            if last_amplifier_output > max {max = last_amplifier_output;};
+//        }
+//    println!("Highest signal that can be sent to the thrusters: {}", max);
+//}
 
 fn main() {
-    part2();
+    part1();
 }
